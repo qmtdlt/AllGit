31c6254a071d860f2dd550e2c79b61a9bf578d61
qmtdlt <qmtdlt@gmail.com>
2018-01-16 17:20:50 +0800

    asp.net mvc 上传下载文件

---
+++ UseGitTips/Mvc/MvcWebSite/MvcApp/MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpPost]
+        public ActionResult Upload(FormCollection form)
+        {
+            if (Request.Files.Count == 0)
+            {
+                //Request.Files.Count 文件数为0上传不成功
+                return View();
+            }
+            var file = Request.Files[0];
+            if (file.ContentLength == 0)
+            {
+                //文件大小大（以字节为单位）为0时，做一些操作
+                return View();
+            }
+            else
+            {
+                //文件大小不为0
+                file = Request.Files[0];
+                //保存成自己的文件全路径,newfile就是你上传后保存的文件,
+                //服务器上的UpLoadFile文件夹必须有读写权限
+                string target = Server.MapPath("/") + ("/UpLoad/");//取得目标文件夹的路径
+                string filename = file.FileName;//取得文件名字
+                string path = target + filename;//获取存储的目标地址
+                file.SaveAs(path);
+            }
+            return View();
+        }
+
+        public ActionResult Download(string filePath, string fileName)
+        {
+            Encoding encoding;
+            string outputFileName = null;
+            fileName = fileName.Replace("'", "");
+            filePath = filePath + fileName;
+
+            string browser = Request.UserAgent.ToUpper();
+            if (browser.Contains("MS") == true && browser.Contains("IE") == true)
+            {
+                outputFileName = HttpUtility.UrlEncode(fileName);
+                encoding = Encoding.Default;
+            }
+            else if (browser.Contains("FIREFOX") == true)
+            {
+                outputFileName = fileName;
+                encoding = Encoding.GetEncoding("GB2312");
+            }
+            else
+            {
+                outputFileName = HttpUtility.UrlEncode(fileName);
+                encoding = Encoding.Default;
+            }
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+            byte[] bytes = new byte[(int)fs.Length];
+            fs.Read(bytes, 0, bytes.Length);
+            fs.Close();
+            Response.Charset = "UTF-8";
+            Response.ContentType = "application/octet-stream";
+            Response.ContentEncoding = encoding;
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + outputFileName);
+            Response.BinaryWrite(bytes);
+            Response.Flush();
+            Response.End();
+            return new EmptyResult();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
