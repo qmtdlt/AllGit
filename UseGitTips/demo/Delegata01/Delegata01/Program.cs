869702b232fa4883443e876ea29ac451f3342a49
qmtdlt <qmtdlt@gmail.com>
2018-03-16 16:01:00 +0800

    win7 激活

---
+++ UseGitTips/demo/Delegata01/Delegata01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegata01
+{
+    public delegate int TransFormer(int x);
+    class Util
+    {
+        public static void TransFormer(int[] values,TransFormer t)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = t(values[i]);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = { 1, 2, 3 };
+            Util.TransFormer(values, square);
+            foreach (var i in values)
+            {
+                Console.WriteLine(i+"  ");
+            }
+        }
+        static int square(int x)
+        {
+            return x * x;
+        }
+    }
+}
