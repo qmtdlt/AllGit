1ed53c25091dad442bc60ea0e1b6126c19e4e182
qmtdlt <qmtdlt@gmail.com>
2018-03-29 10:38:13 +0800

    commit

--- UseGitTips/demo/ConsoleApplication2/ConsoleApplication2/Program.cs
+++ UseGitTips/demo/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -10,31 +10,37 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            NFineBaseEntities db = new NFineBaseEntities();
-            var entity = db.Message_Center.First(x => x.Id > 0);
-            string s = entity.MessageContent;
-            int i = s.IndexOf("id=")+3;
-            
-            string str = s.Substring(i, entity.MessageContent.Length-i);
-            Console.WriteLine(str);
-            int len = 0;
-            foreach (var item in str)
+            string str = "123456789";
+            if (str[str.Length-1] == ',')
             {
-                if (Convert.ToInt32(item) >= 48 && Convert.ToInt32(item) <= 57)
-                {
-                    len++;
-                    Console.WriteLine(item);
-                }
-                else
-                {
-                    break;
-                }
+                str = str.Substring(0, str.Length - 1);
             }
-            Console.WriteLine("len=");
-            Console.WriteLine(len);
-            str = str.Substring(0, len);
-            string result = System.Text.RegularExpressions.Regex.Replace(str, @"[^0-9]+", "");
-            Console.WriteLine(result);
+            Console.WriteLine(str);
+            //NFineBaseEntities db = new NFineBaseEntities();
+            //var entity = db.Message_Center.First(x => x.Id > 0);
+            //string s = entity.MessageContent;
+            //int i = s.IndexOf("id=")+3;
+            
+            //string str = s.Substring(i, entity.MessageContent.Length-i);
+            //Console.WriteLine(str);
+            //int len = 0;
+            //foreach (var item in str)
+            //{
+            //    if (Convert.ToInt32(item) >= 48 && Convert.ToInt32(item) <= 57)
+            //    {
+            //        len++;
+            //        Console.WriteLine(item);
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine("len=");
+            //Console.WriteLine(len);
+            //str = str.Substring(0, len);
+            //string result = System.Text.RegularExpressions.Regex.Replace(str, @"[^0-9]+", "");
+            //Console.WriteLine(result);
         }
     }
 }
---
+++ UseGitTips/demo/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class showVm
+    {
+        public string uid { get; set; }
+        public string name { get; set; }
+        public string pwd { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<showVm> list = new List<showVm>();
+            NFineBaseEntities db = new NFineBaseEntities();
+            foreach (var userFather in db.Sys_User.OrderBy(x=>x.F_RealName))
+            {
+                foreach (var userSon in db.Sys_UserLogOn)
+                {
+                    if (userFather.F_Id == userSon.F_UserId)
+                    {
+                        showVm vm = new showVm();
+                        vm.uid = userFather.F_Id;
+                        vm.name = userFather.F_RealName;
+                        vm.pwd = userSon.F_UserPassword;
+                        list.Add(vm);
+                    }
+                }
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine("{0} \t ;{1} \t;{2}\t",item.uid,item.name,item.pwd);
+            }
+        }
+    }
+}
