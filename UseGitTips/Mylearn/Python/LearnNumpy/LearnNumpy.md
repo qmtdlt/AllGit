fd0e3bc314f59961b30ddfa105bed5aef67b2d23
qmtdlt <qmtdlt@gmail.com>
2018-04-06 01:44:52 +0800

    0406 learn

---
+++ UseGitTips/Mylearn/Python/LearnNumpy/LearnNumpy.md
@@ -0,0 +1,5 @@
+### array(数组)
+
+1. 导入numpy ： import numpy as np
+2. 创建一个数组： a = np.array([1,2,3])
+3. 
---
+++ UseGitTips/我的项目/TestCode/HellowWorld.java
@@ -0,0 +1,7 @@
+class HellowWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hellow,world");
+	}
+}
